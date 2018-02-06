c4d2d1aa5c0c10a0e453e5811e8093e2e7530534
atsushi130 <atsushi_main@outlook.jp>
2018-02-06 09:04:33 +0900

    Ooops!:scream_cat:

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 }
 
 fn connect() {
-    let key = "xoxb-309683363221-SdQm1iU6UR9bwLSCNqmSsirY".to_string();
+    let key = "".to_string();
 
     let mut handler = TabelogBot::from();
     let r = RtmClient::login_and_run(&key, &mut handler);
