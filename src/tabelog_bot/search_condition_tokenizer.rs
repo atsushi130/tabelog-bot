0fdda18aa0f028fabbe67e50ae92d6ed6ac7eae5
atsushi130 <atsushi_main@outlook.jp>
2018-02-10 14:18:52 +0900

    update tokeinze

--- src/tabelog_bot/search_condition_tokenizer.rs
+++ src/tabelog_bot/search_condition_tokenizer.rs
@@ -4,8 +4,9 @@ pub struct SearchConditionTokenizer;
 impl SearchConditionTokenizer {
 
     pub fn analyze(&self, value: &str) -> (String, String) {
-        println!("{}", value);
-        let splitted: Vec<&str> = value.split(' ').collect();
+
+        let mut splitted: Vec<&str> = value.split(' ').collect();
+        splitted.remove(0);
 
         if let Some(area) = splitted.first() {
             if let Some(word) = splitted.last() {
